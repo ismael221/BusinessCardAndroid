9c40a16df1497911b40f354cdcef7482cf55e261
Ismael Nunes <ismaeldenunes@gmail.com>
2024-10-05 14:47:32 -0300

    Final setup

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -1,29 +1,36 @@
 package com.example.businesscard
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Scaffold
+import androidx.compose.material.icons.sharp.Email
+import androidx.compose.material.icons.sharp.Phone
+import androidx.compose.material.icons.sharp.Share
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.businesscard.ui.theme.BusinessCardTheme
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,6 +39,15 @@ class MainActivity : ComponentActivity() {
         setContent {
             BusinessCardTheme {
                 Surface(modifier = Modifier.fillMaxSize()) {
+                    IconCompose(
+                        stringResource(R.string.name),
+                        stringResource(R.string.title)
+                    )
+                    ContactCompose(
+                        stringResource(R.string.phone),
+                        stringResource(R.string.email),
+                        stringResource(R.string.social)
+                    )
                 }
             }
         }
@@ -73,10 +89,76 @@ fun IconCompose(name: String, title: String, modifier: Modifier = Modifier) {
    }
 }
 
+@Composable
+fun ContactCompose(phone: String, email: String, social: String,modifier: Modifier = Modifier){
+    val phoneIcon = Icons.Sharp.Phone
+    val shareIcon = Icons.Sharp.Share
+    val emailIcon = Icons.Sharp.Email
+
+    Column(
+        verticalArrangement = Arrangement.Bottom,
+        modifier = Modifier
+        .fillMaxSize()
+            .padding(start = 50.dp,bottom = 40.dp)
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .padding(16.dp)
+        ) {
+            Icon(
+                phoneIcon,
+                contentDescription = "Phone Icon",
+                modifier = Modifier
+                    .padding(horizontal = 10.dp)
+            )
+            Text(
+                text = phone,
+            )
+        }
+        Row(
+            modifier = Modifier
+                .padding(16.dp)
+        ) {
+            Icon(
+                emailIcon,
+                contentDescription = "Email Icon",
+                modifier = Modifier
+                    .padding(horizontal = 10.dp)
+            )
+            Text(
+                text = email,
+            )
+        }
+        Row(
+            horizontalArrangement = Arrangement.Absolute.Right,
+            modifier = Modifier
+                    .padding(16.dp)
+        ) {
+            Icon(
+                shareIcon,
+                contentDescription = "Share Icon",
+                modifier = Modifier
+                    .padding(horizontal = 10.dp)
+            )
+            Text(
+                text = social,
+            )
+        }
+    }
+}
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     BusinessCardTheme {
-        IconCompose("Ismael Nunes Campos", "Fullstack Developer")
+        IconCompose(
+            stringResource(R.string.name),
+            stringResource(R.string.title)
+        )
+        ContactCompose(
+            stringResource(R.string.phone),
+            stringResource(R.string.email),
+            stringResource(R.string.social)
+        )
     }
 }
